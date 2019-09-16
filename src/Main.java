8c55cbdc0b093f9f3bb1f7b0f2e56e7821879f3c
AlekseiILitvinov <litvinovai988@gmail.com>
2019-09-16 23:24:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Homework for Week 1. Task: write a program calculating the bonuses depending on the total amount of previous
+ * purchases.
+ * All calculations will be done in roubles (kopeks are discarded, or rounded down).
+ * @author Aleksei Litvinov
+ */
+public class Main {
+	public static void main(String[] args) {
+		int totalPurchases = 9000;
+		int currentPurchase = 2550;
+
+		//
+		Scanner scanner = new Scanner(System.in);
+	}
+}
